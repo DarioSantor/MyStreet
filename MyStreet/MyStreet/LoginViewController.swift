10ddc63a47593e12f01ce4c9bb34f7b19b56bcb3
Correia, Jose Bastos <josebcorreia@deloitte.pt>
2023-03-27 14:03:44 +0100

    tratamento de Login e erros do servidor

--- MyStreet/MyStreet/LoginViewController.swift
+++ MyStreet/MyStreet/LoginViewController.swift
@@ -10,8 +10,6 @@ import Firebase
 
 class LoginViewController: UIViewController {
     
-    //test
-    
     private let logo = UIImageView(image: UIImage(named: "logo"))
     
     private var isUserAdmin: Bool = false
@@ -20,11 +18,20 @@ class LoginViewController: UIViewController {
     private let loginButton = CustomButton(title: "LOGIN", bgColor: .systemGreen)
     private let registerButton = CustomButton(title: "REGISTAR", bgColor: .systemBlue)
     
+    private let loginStatusLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Conectando..."
+        label.isHidden = true
+        return label
+    }()
+    
     private let registerQuestion: UILabel = {
         let label = UILabel()
         label.text = "Não tem conta?"
         return label
     }()
+    
+    
 
     override func viewDidLoad() {
         
@@ -37,6 +44,7 @@ class LoginViewController: UIViewController {
         view.addSubview(emailLoginField)
         view.addSubview(passwordLoginField)
         view.addSubview(loginButton)
+        view.addSubview(loginStatusLabel)
         view.addSubview(registerQuestion)
         view.addSubview(registerButton)
         
@@ -52,6 +60,7 @@ class LoginViewController: UIViewController {
         emailLoginField.translatesAutoresizingMaskIntoConstraints = false
         passwordLoginField.translatesAutoresizingMaskIntoConstraints = false
         loginButton.translatesAutoresizingMaskIntoConstraints = false
+        loginStatusLabel.translatesAutoresizingMaskIntoConstraints = false
         registerQuestion.translatesAutoresizingMaskIntoConstraints = false
         registerButton.translatesAutoresizingMaskIntoConstraints = false
         
@@ -76,7 +85,10 @@ class LoginViewController: UIViewController {
             loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             loginButton.heightAnchor.constraint(equalToConstant: 40),
             
-            registerQuestion.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 20),
+            loginStatusLabel.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 15),
+            loginStatusLabel.centerXAnchor.constraint(equalTo: logo.centerXAnchor),
+            
+            registerQuestion.topAnchor.constraint(equalTo: loginStatusLabel.bottomAnchor, constant: 40),
             registerQuestion.centerXAnchor.constraint(equalTo: logo.centerXAnchor),
             
             registerButton.topAnchor.constraint(equalTo: registerQuestion.bottomAnchor, constant: 5),
@@ -94,13 +106,30 @@ class LoginViewController: UIViewController {
         guard let password = self.passwordLoginField.text else { return }
         
         Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            self.loginStatusLabel.text = "Conectando..."
+            self.loginStatusLabel.textColor = .label
+            self.loginStatusLabel.isHidden = false
+
             if let error = error {
                 
                 // TODO: tratamento de erros do servidor
                 print("DEBUG - \(error.localizedDescription)")
+                
+                if (!self.emailLoginField.hasText || !self.passwordLoginField.hasText) {
+                    
+                    self.loginStatusLabel.text = "Indique Email e Password!"
+                }
+                else {
+                    self.loginStatusLabel.text = "Credenciais não coincidem, tente novamente..."
+                }
+                self.loginStatusLabel.textColor = .systemRed
+                self.loginStatusLabel.isHidden = false
                 return
             }
             print("You're IIIIIINNNNNNNN")
+            self.loginStatusLabel.text = "Autenticado!"
+            self.loginStatusLabel.textColor = .label
+            self.loginStatusLabel.isHidden = false
             guard let uid = result?.user.uid else { return }
             
             // guarda o uid do user nos userDefaults
