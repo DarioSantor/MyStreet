9fbcfbdc9f63e5848706e942d6cf4e4f1ed8b4cc
Correia, Jose Bastos <josebcorreia@deloitte.pt>
2023-04-03 21:13:40 +0100

    OccurrenceService add, need to fix request for occ

--- MyStreet/MyStreet/Model/Ocurrence.swift
+++ MyStreet/MyStreet/Model/Ocurrence.swift
@@ -17,11 +17,47 @@ struct Occurrence {
     let description: String
 }
 
-var occurrences = [Occurrence(title:"Lâmpada partida", location: "Rua da Frente", type: "Iluminação Pública", description: "Lâmpada partida"),
-                   Occurrence(title:"Lâmpada partida", location: "Rua da Frente", type: "Iluminação Pública", description: "Lâmpada partida"),
-                   Occurrence(title:"Lâmpada partida", location: "Rua da Frente", type: "Iluminação Pública", description: "Lâmpada partida"),
-                   Occurrence(title:"Lâmpada partida", location: "Rua da Frente", type: "Iluminação Pública", description: "Lâmpada partida"),
-                   Occurrence(title:"Lâmpada partida", location: "Rua da Frente", type: "Iluminação Pública", description: "Lâmpada partida"),
-                   Occurrence(title:"Lâmpada partida", location: "Rua da Frente", type: "Iluminação Pública", description: "Lâmpada partida"),
-                   Occurrence(title:"Lâmpada partida", location: "Rua da Frente", type: "Iluminação Pública", description: "Lâmpada partida"),
-                   Occurrence(title:"Lâmpada partida", location: "Rua da Frente", type: "Iluminação Pública", description: "Lâmpada partida")]
+class OccurrenceService {
+
+    public static var occurrences: [Occurrence] = []
+
+    init() {
+        getOccurrencesFromDatabase { occurrences in
+            OccurrenceService.occurrences = occurrences
+        }
+        
+        
+        print("getOccurrencesFromDatabase")
+        print(OccurrenceService.occurrences)
+    }
+    
+    // MARK: - func to get all Occurences at FireBase
+    public func getOccurrencesFromDatabase(completionHandler: @escaping ([Occurrence]) -> Void) {
+        REF_OCCURRENCES.observeSingleEvent(of: .value) { (snapshot) in
+            var occurrences = [Occurrence]()
+            guard let snapshotValue = snapshot.value as? [String: Any] else {
+                print("Snapshot value is nil")
+                return
+            }
+            
+            // TODO: - ir buscar os multiplos subvalores
+            
+            for occurrence in snapshotValue {
+//                occurrenceData = REF_OCCURRENCES.child(key)
+                
+                guard let occurrenceData = occurrence.value as? [String: Any],
+                      let title = occurrenceData["title"] as? String,
+                      let location = occurrenceData["location"] as? String,
+                      let type = occurrenceData["type"] as? String,
+                      let description = occurrenceData["description"] as? String else {
+                    print("Invalid occurrence data")
+                    print("value \(occurrence)")
+                    continue
+                }
+                let newOccurrence = Occurrence(title: title, location: location, type: type, description: description)
+                occurrences.append(newOccurrence)
+            }
+            completionHandler(occurrences)
+        }
+    }
+}
